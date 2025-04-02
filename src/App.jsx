583a10c29c6603e2ff8fa404cb9f65bcbd6d2395
Shreyash Tripathi <shreyashtripathi@Shreyashs-MacBook-Air.local>
2025-04-02 15:51:01 +0530

    uploaded Neo code - pending : responsiveness

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import LoginPage from './LoginPage';
+import Profile from './Profile';
+import Navbar from './Navbar';
+import './App.css';
+import './index.css';
+
+function App() {
+  const [walletAddress, setWalletAddress] = useState('');
+  const [account, setAccount] = useState('');
+
+  const handleWalletConnect = (address) => {
+    setWalletAddress(address);
+    // You can add additional logic here
+    console.log('Connected address:', address);
+  };
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-green-500 via-gray-900 to-black ">
+      <Navbar account={account}/>
+      <LoginPage onConnect={handleWalletConnect} setAccount={setAccount} account={account}/>
+    </div>
+  );
+}
+
+export default App;
